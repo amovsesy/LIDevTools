b8086509c23d7b7e3a6c1a7c7f2e216aeb0afada
Aleksandr Movsesyan <amovsesy@amovsesy-mn1.linkedin.biz>
2013-08-06 13:49:43 -0700

    Changed names

---
+++ DevTools/JiraViewController.m
@@ -0,0 +1,29 @@
+//
+//  JiraViewController.m
+//  DevTools
+//
+//  Created by Aleksandr Movsesyan on 8/6/13.
+//  Copyright (c) 2013 Aleksandr Movsesyan. All rights reserved.
+//
+
+#import "JiraViewController.h"
+
+@interface JiraViewController ()
+
+@end
+
+@implementation JiraViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DevTools/ReviewBoardViewController.m
@@ -0,0 +1,29 @@
+//
+//  ReviewBoardViewController.m
+//  DevTools
+//
+//  Created by Aleksandr Movsesyan on 8/6/13.
+//  Copyright (c) 2013 Aleksandr Movsesyan. All rights reserved.
+//
+
+#import "ReviewBoardViewController.h"
+
+@interface ReviewBoardViewController ()
+
+@end
+
+@implementation ReviewBoardViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
